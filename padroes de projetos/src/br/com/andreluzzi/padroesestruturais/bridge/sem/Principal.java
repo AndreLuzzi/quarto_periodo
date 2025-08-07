b93d812660d648e54b45eb5847596848c8c68985
Andre Cappellesso Luzzi <andreluzzi@outlook.com.br>
2025-08-06 21:37:59 -0300

    realizado em sala

---
+++ padroes de projetos/src/br/com/andreluzzi/padroesestruturais/bridge/sem/Principal.java
@@ -0,0 +1,14 @@
+package br.com.andreluzzi.padroesestruturais.bridge;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        TVcomControleBasico tvbasica = new TVcomControleBasico();
+        tvbasica.ligar();
+        tvbasica.aumentarVolume();
+
+        RadioComControleAvancado radioAvancado = new RadioComControleAvancado();
+        radioAvancado.ligar();
+        radioAvancado.mudarEstacao(102.7);
+    }
+}
--- padroes de projetos/src/br/com/andreluzzi/padroesestruturais/bridge/sem/TVcomControleBasico.java
+++ padroes de projetos/src/br/com/andreluzzi/padroesestruturais/bridge/sem/TVcomControleBasico.java
@@ -5,9 +5,11 @@ public class TVcomControleBasico {
     public void ligar(){
         System.out.println("TV Ligada");
     }
+
     public void desligar(){
         System.out.println("TV Desligada");
     }
+    
     public void aumentarVolume(){
         System.out.println("Volume da TV aumentada");
     }
